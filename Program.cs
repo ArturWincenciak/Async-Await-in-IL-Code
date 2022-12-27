f25ea274df6df126a1e2613b269eaf754ac1c116
TeoVincent <artur.wincenciak@gmail.com>
2022-12-27 14:10:52 +0100

    Log time now method

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 ﻿Console.WriteLine("Alright Async Await!");
 
-Console.WriteLine($"01 {DateTime.Now:O} Global");
+Console.WriteLine($"{Log.TimeNow} 01 Global");
 var joy = new Joy();
 await joy.Example();
-Console.WriteLine($"02 {DateTime.Now:O} Global");
+Console.WriteLine($"{Log.TimeNow} 02 Global");
 
 internal class Joy
 {
     public async Task Example()
     {
-        Console.WriteLine($"01 {DateTime.Now:O} {nameof(Example)}");
+        Console.WriteLine($"{Log.TimeNow} 01 {nameof(Example)}");
         await Task.Delay(TimeSpan.FromSeconds(5));
-        Console.WriteLine($"02 {DateTime.Now:O} {nameof(Example)}");
+        Console.WriteLine($"{Log.TimeNow} 02 {nameof(Example)}");
     }
+}
+
+internal static class Log
+{
+    public static string TimeNow => 
+        $"{DateTime.Now:HH:mm:ss.fff}";
 }
