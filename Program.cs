86fe6b7cdcd954cc8335c20adb2b483fdf1d054a
TeoVincent <artur.wincenciak@gmail.com>
2022-12-27 13:49:18 +0100

    Joy example

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿Console.WriteLine("Alright Async Await!");
+
+Console.WriteLine($"01 {DateTime.Now:O} Global");
+var joy = new Joy();
+await joy.Example();
+Console.WriteLine($"02 {DateTime.Now:O} Global");
+
+internal class Joy
+{
+    public async Task Example()
+    {
+        Console.WriteLine($"01 {DateTime.Now:O} {nameof(Example)}");
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        Console.WriteLine($"02 {DateTime.Now:O} {nameof(Example)}");
+    }
+}
